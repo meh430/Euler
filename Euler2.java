876f13c6c4a1fcab2ebbaa68d7a4e0deec756a71
meh430 <50446519+meh430@users.noreply.github.com>
2019-07-25 20:08:12 -0400

    Create Euler2.java

---
+++ Euler2.java
@@ -0,0 +1,22 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    long t1 = 1, t2 = 2, sum = 0, sumev = 0;
+	    for (long i = 0; i < 100; i++) {
+	        sum = t1 + t2;
+	        t1 = t2;
+	        t2 = sum;
+	        if (t1 % 2 == 0) {
+	            sumev += t1;
+	        }
+	        if (sum > 4000000) {
+	            break;
+	        }
+	    }
+	    System.out.println(t1);
+	    System.out.println(sumev);
+	}
+	
+	
+}
+
