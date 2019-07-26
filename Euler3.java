bbafb9abce65a2b11347b1e49c216773ac1dc55e
meh430 <50446519+meh430@users.noreply.github.com>
2019-07-25 20:08:50 -0400

    Create Euler3.java

---
+++ Euler3.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main (String[]args) {
+        ArrayList<Long> factors = new ArrayList<Long>();
+		long limit = 600851475143L;
+        
+        for (long i = 1; i < limit; i++) {
+			if (checkPrime(i) && limit % i == 0)  {
+				factors.add(i);
+				System.out.println(i);
+			}	
+		}
+		
+		System.out.println(factors);
+		System.out.println(Collections.max(factors));        
+    }
+
+	static boolean checkPrime(long num){
+		if(num > 2){
+			for(long i = 2; i <= Math.sqrt(num); i++){
+				if(num % i == 0)
+					return false;
+			}
+			return true;
+		} else
+			return true;
+		}
+}
+
+
