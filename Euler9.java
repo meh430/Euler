07c146e8b53be90d04cce8d4e27a295365f414a4
meh430 <50446519+meh430@users.noreply.github.com>
2019-07-28 19:52:23 -0400

    Create Euler9.java

---
+++ Euler9.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main (String[]args) {
+		int c = 0;
+		for (int a = 3; a < 500; a+=3) {
+			for (int b = 4; b < 500; b+=4) {
+				c = 1000 - (a + b);
+				if ((a*a) + (b*b) == c*c) {
+					System.out.println("a: " + a + ", b: " + b + ", c: " + c);
+					System.out.println(a * b * c);
+				}
+			}
+		}
+	}
+}
+
