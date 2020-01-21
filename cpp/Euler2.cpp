731b19e1dd5416a9d75c84b81e03c3117f4d96ad
meh430 <50446519+meh430@users.noreply.github.com>
2020-01-21 09:02:26 -0500

    Create Euler2.cpp

---
+++ cpp/Euler2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main () {
+    long t1 = 1, t2 = 2, sum = 0, sumev = 0;
+    for (long i = 0; i < 100; i++) {
+        sum = t1 + t2;
+        t1 = t2;
+        t2 = sum;
+        if (t1 % 2 == 0) {
+	        sumev += t1;
+        }
+        if (sum > 4000000) {
+	        break;
+	    }
+    }
+    
+    std::cout << t1 << std::endl;
+    std::cout << sumev << std::endl;
+}
